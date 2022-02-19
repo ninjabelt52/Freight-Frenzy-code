1d8ef13a5c44fe480b31d9b0b199503baa241bff
bkelly <you@example.com>
2022-02-18 19:01:18 -0600

    Added Measuring tape code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/TapeMeasure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/TapeMeasure.java
@@ -1,24 +1,139 @@
 package org.firstinspires.ftc.teamcode.classes;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class TapeMeasure {
+import java.io.File;
 
-    private Servo liftL, liftR, zAxis, xAxis, feedL, feedR;
+@Config
+public class TapeMeasure implements Runnable{
+
+    private Servo liftL, liftR, zAxis, xAxis;
+    private CRServo  feed;
     public static double liftInit = 0, zInit = 0, xInit = 0;
+    public static double liftOpen = 1, xOpen = 1;
+    public static double targetX = 36, targetY = 36;
+    public static double leftBound = 0, rightBound = 1;
+    private double lastAngle = 0, globalAngle = 0, baseAngle = 0;
+    private boolean toggle;
+    BNO055IMU imu;
+    private boolean isOpen = false;
+    Gamepad gamepad1;
+    String telemetry;
 
-    public TapeMeasure(HardwareMap hardwareMap){
+    public TapeMeasure(HardwareMap hardwareMap, Gamepad gamepadOne){
         liftL = hardwareMap.get(Servo.class, "liftL");
         liftR = hardwareMap.get(Servo.class, "liftR");
         zAxis = hardwareMap.get(Servo.class, "zAxis");
         xAxis = hardwareMap.get(Servo.class, "xAxis");
-        feedL = hardwareMap.get(Servo.class, "feedL");
-        feedR = hardwareMap.get(Servo.class, "feedR");
+        feed = hardwareMap.get(CRServo.class, "feed");
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu.initialize(parameters);
+
+        zAxis.scaleRange(leftBound, rightBound);
+
+        gamepad1 = gamepadOne;
 
         liftL.setPosition(liftInit);
         liftR.setPosition(liftInit);
         zAxis.setPosition(zInit);
         xAxis.setPosition(xInit);
+
+        while(!imu.isGyroCalibrated()){
+            telemetry = "gyro is not calibrated";
+        }
+
+        telemetry = "gyro is calibrated";
+    }
+
+    public void open (){
+        liftL.setPosition(liftOpen);
+        liftR.setPosition(liftOpen);
+        xAxis.setPosition(xOpen);
+        zAxis.setPosition(Math.atan2(targetX, targetY) - robotRotation() / 180);
+        isOpen = true;
+    }
+
+    public void close(){
+        liftL.setPosition(liftInit);
+        liftR.setPosition(liftInit);
+        xAxis.setPosition(xInit);
+        isOpen = false;
+    }
+
+    public void extend(double power){
+        feed.setPower(power);
+    }
+
+    public boolean isOpen(){
+        return isOpen;
+    }
+
+    public void alternate(){
+        if(isOpen){
+            close();
+        }else{
+            open();
+        }
+    }
+
+    public double robotRotation(){
+        double heading = imu.getAngularOrientation().secondAngle;
+
+        double deltaAngle = heading - lastAngle;
+
+        if(lastAngle < -180){
+            deltaAngle += 360;
+        }else if(lastAngle > 180){
+            deltaAngle -= 360;
+        }
+
+        globalAngle += deltaAngle;
+
+        lastAngle = heading;
+
+        return globalAngle - baseAngle;
+    }
+
+    public void resetRobotRotation(){
+        baseAngle = robotRotation();
+    }
+
+    public String toString(){
+        return telemetry;
+    }
+
+    public void run(){
+        while(!Thread.interrupted()){
+            if(gamepad1.start){
+                if(toggle){
+                    alternate();
+                    toggle = false;
+                }
+            }else{
+                toggle = true;
+            }
+
+//          TODO: Need to add code that will only extend the tape when the system is up,
+//            and only retract the tape otherwise.
+            if(gamepad1.dpad_up){
+                extend(1);
+            }else if(gamepad1.dpad_down){
+                extend(-1);
+            }else{
+                extend(0);
+            }
+
+            telemetry = "" + robotRotation();
+        }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MeasuringTapeTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.classes.TapeMeasure;
+
+
+@TeleOp
+public class MeasuringTapeTest extends LinearOpMode {
+    public void runOpMode(){
+        TapeMeasure tape = new TapeMeasure(hardwareMap, gamepad1);
+        boolean toggle = false;
+
+        Thread tapeThread = new Thread(tape);
+
+        telemetry.addData("Tape Telemetry", tape);
+        telemetry.update();
+
+        waitForStart();
+        tapeThread.start();
+        while (opModeIsActive()){
+            telemetry.addData("Tape Telemetry", tape);
+            telemetry.update();
+        }
+        tapeThread.interrupt();
+    }
+}
