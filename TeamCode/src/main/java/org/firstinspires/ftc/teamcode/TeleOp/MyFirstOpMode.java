323fc0dba0fdcf40373793b90c75445f88f3501d
GithubXavi <Xavienkonig@gmail.com>
2022-05-31 17:27:37 -0500

    Made a forward code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MyFirstOpMode.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class MyFirstOpMode extends LinearOpMode {
+
+    DcMotor frontleftMotor;
+    DcMotor frontrightMotor;
+    DcMotor backleftMotor;
+    DcMotor backrightMotor;
+
+    double power = 0.5;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        frontleftMotor = hardwareMap.dcMotor.get("FrontLeft_Motor");
+        frontrightMotor = hardwareMap.dcMotor.get("FrontRight_Motor");
+        backleftMotor = hardwareMap.dcMotor.get("BackLeft_Motor");
+        backrightMotor = hardwareMap.dcMotor.get("BackRight_Motor");
+        waitForStart();
+       while (opModeIsActive()) {
+        frontleftMotor.setPower(power);
+        frontrightMotor.setPower(power);
+        backleftMotor.setPower(power);
+        backrightMotor.setPower(power);
+
+        sleep(2000);
+
+        power = 0.0;
+
+        frontleftMotor.setPower(power);
+        frontrightMotor.setPower(power);
+        backleftMotor.setPower(power);
+        backrightMotor.setPower(power);
+        }
+    }
+}
