d3936bf9535bbfa7cc87e26d36a87893da753a6f
GithubXavi <Xavienkonig@gmail.com>
2022-06-02 14:27:04 -0500

    Made a right code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MyFirstOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MyFirstOpMode.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class MyFirstOpMode extends LinearOpMode {
-
+// Define variables
     DcMotor frontleftMotor;
     DcMotor frontrightMotor;
     DcMotor backleftMotor;
@@ -21,7 +21,7 @@ public class MyFirstOpMode extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+// Initialization
         frontleftMotor = hardwareMap.dcMotor.get("fl");
         frontrightMotor = hardwareMap.dcMotor.get("fr");
         backleftMotor = hardwareMap.dcMotor.get("bl");
@@ -34,7 +34,7 @@ public class MyFirstOpMode extends LinearOpMode {
 
         frontleftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backleftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
+// IMU calibration
         while (!IMU.isGyroCalibrated()) {
             telemetry.addData("status","IMU is not calibrated");
             telemetry.update();
@@ -42,19 +42,20 @@ public class MyFirstOpMode extends LinearOpMode {
         }
         telemetry.addData("status","IMU is calibrated");
         telemetry.update();
-
+// Run
         waitForStart();
        while (opModeIsActive()) {
+// Motion
         frontleftMotor.setPower(power+strafe+rotation);
         frontrightMotor.setPower(power-strafe-rotation);
         backleftMotor.setPower(power-strafe+rotation);
         backrightMotor.setPower(power+strafe-rotation);
 
-
+// Set movement variables
         power = gamepad1.left_stick_y;
         rotation = gamepad1.right_stick_x;
         strafe = -gamepad1.left_stick_x;
-
+// Update X, Y, and Z variables
         telemetry.addData("X axis rotation",IMU.getAngularOrientation().firstAngle);
         telemetry.addData("Y axis rotation",IMU.getAngularOrientation().secondAngle);
         telemetry.addData("Z axis rotation",IMU.getAngularOrientation().thirdAngle);
