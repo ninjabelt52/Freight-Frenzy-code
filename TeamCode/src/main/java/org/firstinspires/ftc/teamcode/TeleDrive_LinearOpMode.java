160a150c7da9a7e080dd7e62239211647239659c
GithubXavi <Xavienkonig@gmail.com>
2022-08-12 17:32:15 -0500

    Says static is not allowed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive_LinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive_LinearOpMode.java
@@ -339,6 +339,9 @@ public class TeleDrive_LinearOpMode extends LinearOpMode {
 
         //CUSTOM CODE GOES HERE
 
+        static final double INCREMENT   = 0.05;
+        static final double MAX_POS     =  1.0;
+        static final double MIN_POS     =  0.0;
         double gateClosed = .1, gateOpen = .37, bottomClosed = 1, bottomOpen = 0, quasiGateOpen = .2, straight, strafe, rotation;
         int BOTTOM = 175, MIDDLE = 250, HIGH = 350;
         int targetPos = 0;
@@ -372,6 +375,12 @@ public class TeleDrive_LinearOpMode extends LinearOpMode {
                 .addStep(1,1,150)
                 .build();
 
+        Servo   servo;
+        servo = hardwareMap.get(Servo.class, "horizontal");
+        Servo   servo2;
+        servo2 = hardwareMap.get(Servo.class, "vertical");
+        double  position = (MAX_POS - MIN_POS) / 2;
+        double  position2 = (MAX_POS - MIN_POS) / 2;
         bl = hardwareMap.get(DcMotor.class, "bl");
         br = hardwareMap.get(DcMotor.class, "br");
         fl = hardwareMap.get(DcMotor.class, "fl");
@@ -423,7 +432,37 @@ public class TeleDrive_LinearOpMode extends LinearOpMode {
 
             while (opModeIsActive()) {
 
+                if(gamepad1.dpad_left){
+                    position = position-INCREMENT;
+                }
+                if(position<MIN_POS){
+                    position = MIN_POS;
+                }
+                if(gamepad1.dpad_right){
+                    position = position+INCREMENT;
+                }
+                if(position>MAX_POS){
+                    position = MAX_POS;
+                }
 
+                if(gamepad1.dpad_down){
+                    position2 = position2-INCREMENT;
+                }
+                if(position2<MIN_POS){
+                    position2 = MIN_POS;
+                }
+                if(gamepad1.dpad_up){
+                    position2 = position2+INCREMENT;
+                }
+                if(position2>MAX_POS){
+                    position2 = MAX_POS;
+                }
+
+                servo.setPosition(position);
+                servo2.setPosition(position2);
+                telemetry.addData("Horizontal Position", "%5.2f", position);
+                telemetry.addData("Vertical Position", "%5.2f", position2);
+                telemetry.update();
                 }
 
 
