3951d1bed28f9141b124422e0893aac66304456b
GithubXavi <Xavienkonig@gmail.com>
2022-08-30 16:57:02 -0500

    added camera support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive_LinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive_LinearOpMode.java
@@ -1,18 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.classes.TapeMeasure;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -330,8 +320,19 @@ public class TeleDrive_LinearOpMode extends LinearOpMode {
         telemetry.addData("Connect your server to " + address + ":" + port, "");
         telemetry.update();
 
-        waitForStart();
+        final double INCREMENT   = 0.00025;
+        final double HOR_MAX_POS     =  1.0;
+        final double HOR_MIN_POS     =  .15;
+        final double VER_MAX_POS     =  .67;
+        final double VER_MIN_POS     =  .25;
 
+        Servo servo;
+        servo = hardwareMap.get(Servo.class, "horizontal");
+        Servo   servo2;
+        servo2 = hardwareMap.get(Servo.class, "vertical");
+        double  position = .72;
+        double  position2 = .43;
+        waitForStart();
         canRunGamepadThread = true;
 
         startGamepadHandlerThread();
@@ -339,123 +340,34 @@ public class TeleDrive_LinearOpMode extends LinearOpMode {
 
         //CUSTOM CODE GOES HERE
 
-        static final double INCREMENT   = 0.05;
-        static final double MAX_POS     =  1.0;
-        static final double MIN_POS     =  0.0;
-        double gateClosed = .1, gateOpen = .37, bottomClosed = 1, bottomOpen = 0, quasiGateOpen = .2, straight, strafe, rotation;
-        int BOTTOM = 175, MIDDLE = 250, HIGH = 350;
-        int targetPos = 0;
-        int SHARED = 175;
-
-        DcMotor bl, br, fl, fr, intake, arm, armSupport;
-        DcMotorEx duckWheel;
-        Servo gate, bottom;
-        DigitalChannel limit;
-        BNO055IMU imu;
-        double slowDown = 1, armPower = 1;
-        int bottomLimit = 0, level = 0, levelHeight = 350;
-        boolean toggle = false, armState = false, toggle2 = false, toggle3 = false;
-        boolean rumble1 = true, rumble2 = true, rumble3 = true;
-        String currentTarget = "top";
-        TapeMeasure tapeMeasure = new TapeMeasure(hardwareMap, gamepad1,gamepad2, TapeMeasure.Side.BLUE);
-        Thread tapeMeasureThread = new Thread(tapeMeasure);
-
-        ElapsedTime timer = new ElapsedTime();
-        Gamepad.RumbleEffect fastBlip;
-
-        fastBlip = new Gamepad.RumbleEffect.Builder()
-                .addStep(1,1,150)
-                .addStep(0,0,50)
-                .addStep(1,1,150)
-                .addStep(0,0,50)
-                .addStep(1,1,150)
-                .addStep(0,0,50)
-                .addStep(1,1,150)
-                .addStep(0,0,50)
-                .addStep(1,1,150)
-                .build();
-
-        Servo   servo;
-        servo = hardwareMap.get(Servo.class, "horizontal");
-        Servo   servo2;
-        servo2 = hardwareMap.get(Servo.class, "vertical");
-        double  position = (MAX_POS - MIN_POS) / 2;
-        double  position2 = (MAX_POS - MIN_POS) / 2;
-        bl = hardwareMap.get(DcMotor.class, "bl");
-        br = hardwareMap.get(DcMotor.class, "br");
-        fl = hardwareMap.get(DcMotor.class, "fl");
-        fr = hardwareMap.get(DcMotor.class, "fr");
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        duckWheel = hardwareMap.get(DcMotorEx.class, "Rim");
-        arm = hardwareMap.get(DcMotor.class, "arm");
-        armSupport = hardwareMap.get(DcMotor.class, "arm2");
-        gate = hardwareMap.get(Servo.class, "gate");
-        bottom = hardwareMap.get(Servo.class, "bottom");
-        limit = hardwareMap.get(DigitalChannel.class, "limit");
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-
-        fl.setDirection(DcMotorSimple.Direction.REVERSE);
-        bl.setDirection(DcMotorSimple.Direction.REVERSE);
-        intake.setDirection(DcMotorSimple.Direction.REVERSE);
-        armSupport.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        armSupport.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        duckWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armSupport.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        limit.setMode(DigitalChannel.Mode.INPUT);
-
-        gate.scaleRange(0,1);
-        bottom.scaleRange(0,1);
-
-        imu.initialize(parameters);
-
-        while(!isStopRequested() && !imu.isGyroCalibrated()){
-            telemetry.addLine("not initialized");
-            telemetry.update();
-        }
-
-        telemetry.addLine("Waiting for start");
-        telemetry.update();
-
         waitForStart();
-
-        tapeMeasureThread.start();
-        timer.reset();
-
-
+        if (opModeIsActive()) {
             while (opModeIsActive()) {
 
                 if(gamepad1.dpad_left){
                     position = position-INCREMENT;
                 }
-                if(position<MIN_POS){
-                    position = MIN_POS;
+                if(position<HOR_MIN_POS){
+                    position = HOR_MIN_POS;
                 }
                 if(gamepad1.dpad_right){
                     position = position+INCREMENT;
                 }
-                if(position>MAX_POS){
-                    position = MAX_POS;
+                if(position>HOR_MAX_POS){
+                    position = HOR_MAX_POS;
                 }
 
                 if(gamepad1.dpad_down){
                     position2 = position2-INCREMENT;
                 }
-                if(position2<MIN_POS){
-                    position2 = MIN_POS;
+                if(position2<VER_MIN_POS){
+                    position2 = VER_MIN_POS;
                 }
                 if(gamepad1.dpad_up){
                     position2 = position2+INCREMENT;
                 }
-                if(position2>MAX_POS){
-                    position2 = MAX_POS;
+                if(position2>VER_MAX_POS){
+                    position2 = VER_MAX_POS;
                 }
 
                 servo.setPosition(position);
@@ -463,11 +375,11 @@ public class TeleDrive_LinearOpMode extends LinearOpMode {
                 telemetry.addData("Horizontal Position", "%5.2f", position);
                 telemetry.addData("Vertical Position", "%5.2f", position2);
                 telemetry.update();
-                }
 
 
             }
             canRunGamepadThread = false;
             socket.close();
+        }
     }
 }
