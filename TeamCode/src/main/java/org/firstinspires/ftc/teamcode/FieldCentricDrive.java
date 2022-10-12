6d26dff6f7e6234cd796260324e34d9ed1fd94e9
Teon <tnfallavol@gmail.com>
2022-10-11 21:45:40 -0500

    More math on Field Centric V4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -147,10 +147,10 @@ public class FieldCentricDrive extends LinearOpMode {
             double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
             double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
             double rightX = gamepad1.right_stick_x;
-            final double v1 = r * Math.cos(robotAngle - heading) + rightX;
-            final double v2 = r * Math.sin(robotAngle - heading) - rightX;
-            final double v3 = r * Math.sin(robotAngle - heading) + rightX;
-            final double v4 = r * Math.cos(robotAngle - heading) - rightX;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
 
             leftFrontDrive.setPower(v1);
             rightFrontDrive.setPower(v2);
